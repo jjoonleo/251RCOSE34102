91141c383342c14103fa70e895112ecfa828a6a5
jjoonleo <jjoonleo@gmail.com>
2025-05-14 13:53:12 +0900

    feat: define IOBurst structure and add print function prototype

--- include/process.h
+++ include/process.h
@@ -6,18 +6,22 @@
 
 #define MAX_IO_BURSTS_COUNT 5
 
-typedef struct
+typedef struct IOBurst
 {
   int io_request_time;
   int io_burst_time;
+  void (*print)(struct IOBurst *);
 } IOBurst;
 
 IOBurst *new_io_burst(int io_request_time, int io_burst_time);
 
+// Function to print an IOBurst
+void print_ioburst(IOBurst *io_burst);
+
 // Function to compare two IOBurst structures by io_request_time
 int compare_io_burst(const void *a, const void *b);
 
-typedef struct
+typedef struct Process
 {
   int pid;
   int arrival_time;
@@ -27,8 +31,12 @@ typedef struct
   int waiting_time;
   int turnaround_time;
   LinkedList *io_burst_list; // Linked list to store IO burst times
+  void (*print)(struct Process *);
 } Process;
 
 Process *new_process(int pid);
+
+// Function to print a Process
+void print_process(Process *process);
 
 #endif // PROCESS_H
