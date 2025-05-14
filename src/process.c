48729094eed76bacfd1a9dd6c3db27d07ef69cb0
jjoonleo <jjoonleo@gmail.com>
2025-05-14 13:53:24 +0900

    feat: add print function for IOBurst and enhance process printing functionality

--- src/process.c
+++ src/process.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
+
+#define min(x, y) (x) < (y) ? (x) : (y)
+
+#define max(x, y) (x) > (y) ? (x) : (y)
 
 // Function to compare two IOBurst structures by io_request_time
 int compare_io_burst(const void *a, const void *b)
@@ -26,6 +31,13 @@ IOBurst *new_io_burst(int io_request_time, int io_burst_time)
   return io_burst;
 }
 
+// Function to print an IOBurst
+void print_ioburst(IOBurst *io_burst)
+{
+  if (io_burst)
+    printf("IO Request Time: %d, IO Burst Time: %d\n", io_burst->io_request_time, io_burst->io_burst_time);
+}
+
 Process *new_process(int pid)
 {
   Process *p = (Process *)malloc(sizeof(Process));
@@ -40,13 +52,25 @@ Process *new_process(int pid)
   p->io_burst_list = createLinkedList(); // Initialize the IO burst linked list
 
   // Add random IO burst times in ascending order
-  int num_io_bursts = rand() % MAX_IO_BURSTS_COUNT + 1; // Random number of IO bursts (0 to 4)
-  printf("Process %d: Number of IO Bursts: %d\n", p->pid, num_io_bursts);
+  int num_io_bursts = rand() % min(MAX_IO_BURSTS_COUNT, p->cpu_burst - 1) + 1; // Random number of IO bursts (0 to 4)
   for (int i = 0; i < num_io_bursts; i++)
   {
-    IOBurst *io_burst = new_io_burst(rand() % p->cpu_burst + 1, (rand() % 5) + 1); // Random IO request time and burst time
+    IOBurst *io_burst = new_io_burst(rand() % (p->cpu_burst - 1) + 1, (rand() % 5) + 1); // Random IO request time and burst time
     p->io_burst_list->insertInOrder(p->io_burst_list, (void *)io_burst, compare_io_burst);
   }
+  p->io_burst_list->removeDuplicates(p->io_burst_list, compare_io_burst); // Remove duplicates if any
 
+  p->print = print_process; // Assign the print function
   return p;
+}
+
+// Function to print a Process
+void print_process(Process *process)
+{
+  if (!process)
+    return;
+  printf("Process %d: Arrival Time: %d, CPU Burst: %d, Priority: %d\n", process->pid, process->arrival_time, process->cpu_burst, process->priority);
+  printf("IO Bursts:\n");
+  if (process->io_burst_list && process->io_burst_list->print)
+    process->io_burst_list->print(process->io_burst_list, (void (*)(void *))print_ioburst);
 }
